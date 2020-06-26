b024b77bfeedb75baa26a79e9aca81f3304c5a4e
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-26 11:29:07 -0400

    Arreglando el error al loguearse

--- src/main/java/com/example/demo/controller/AppController.java
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -2,33 +2,27 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class DemoController {
+public class AppController {
 
-    /*@GetMapping
-    public String index(){
-        return "<b>Klk con Klk</b>";
-    }*/
-
-    @GetMapping({"/", "/login"})
-    public String index(){
+    @GetMapping({"/","/login"})
+    public String index() {
         return "index";
     }
 
     @GetMapping("/menu")
-    public String menu(){
+    public String menu() {
         return "menu";
     }
 
     @GetMapping("/user")
-    public String user(){
+    public String user() {
         return "user";
     }
 
     @GetMapping("/admin")
-    public String admin(){
+    public String admin() {
         return "admin";
     }
 
