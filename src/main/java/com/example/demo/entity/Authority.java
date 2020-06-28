d9db339763509024e9b1588e0b29d9954c1e6a0a
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-28 16:46:30 -0400

    Creando las tablas en la BD desde la app

---
+++ src/main/java/com/example/demo/entity/Authority.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String authority;
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+}
